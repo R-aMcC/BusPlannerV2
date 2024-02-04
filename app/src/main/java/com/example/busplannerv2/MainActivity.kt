2970f31182c57f86dc82c6a4f98defab04707f3c
Ryan McCracken <ryangamingdab@gmail.com>
2024-02-04 08:52:01 -0500

    Now checks to see if stop and route numbers are the proper length

--- app/src/main/java/com/example/busplannerv2/MainActivity.kt
+++ app/src/main/java/com/example/busplannerv2/MainActivity.kt
@@ -44,110 +44,133 @@ class MainActivity : AppCompatActivity() {
 
             val stopTextEdit = findViewById<EditText>(R.id.stopNumber)
             val stopTextContent = stopTextEdit.text
-
             val routeTextEdit = findViewById<EditText>(R.id.routeNumber)
             val routeTextContent = routeTextEdit.text
-            Log.d("TextContent", "$stopTextContent")
-            val retrofitData = retrofitBuilder.getData(stopTextContent .toString(), routeTextContent.toString())
 
-            retrofitData.enqueue(object : Callback<BusAPI?> {
-                override fun onResponse(call: Call<BusAPI?>, response: Response<BusAPI?>) {
-                    val responseBody = response.body()!!
-                    val myStringBuilder = StringBuilder()
-                    val stopName = responseBody.GetNextTripsForStopResult.StopLabel
-                    val stopNo = responseBody.GetNextTripsForStopResult.StopNo
-                    Log.d("JSON Object", "${responseBody.GetNextTripsForStopResult.Route}")
-                    val Route = responseBody.GetNextTripsForStopResult.Route.toString()
-                    val RouteJSON = responseBody.GetNextTripsForStopResult.Route
-                    if(Route[18].toString() == "["){
-                        try{
+            if(stopTextContent.length != 4){
+                Log.d("stop != 4", "Stop is not equal to 4")
+                myString.append("Please enter a valid stop number (4 numbers)")
+                updateText.text = myString
+
+            }else if(routeTextContent.length > 3){
+                myString.append("Please enter a valid route number ( 3 or less numbers)")
+                updateText.text = myString
+            }else {
+
+                Log.d("TextContent", "$stopTextContent")
+                val retrofitData =
+                    retrofitBuilder.getData(stopTextContent.toString(), routeTextContent.toString())
+
+                retrofitData.enqueue(object : Callback<BusAPI?> {
+                    override fun onResponse(call: Call<BusAPI?>, response: Response<BusAPI?>) {
+                        val responseBody = response.body()!!
+                        val myStringBuilder = StringBuilder()
+                        val stopName = responseBody.GetNextTripsForStopResult.StopLabel
+                        val stopNo = responseBody.GetNextTripsForStopResult.StopNo
+                        Log.d("JSON Object", "${responseBody.GetNextTripsForStopResult.Route}")
+                        val Route = responseBody.GetNextTripsForStopResult.Route.toString()
+                        val RouteJSON = responseBody.GetNextTripsForStopResult.Route
+                        if (Route[18].toString() == "[") {
+                            try {
 
 
-                            val data = Gson().fromJson(responseBody.GetNextTripsForStopResult.Route, RouteList::class.java)
+                                val data = Gson().fromJson(
+                                    responseBody.GetNextTripsForStopResult.Route,
+                                    RouteList::class.java
+                                )
 
-                            Log.d("data", "${data.RouteDirection}")
+                                Log.d("data", "${data.RouteDirection}")
 
-                            for(routes in data.RouteDirection){
-                                val route = routes.RouteLabel
-                                val routeNo = routes.RouteNo
+                                for (routes in data.RouteDirection) {
+                                    val route = routes.RouteLabel
+                                    val routeNo = routes.RouteNo
 
+
+
+                                    Log.d("responseBody", "$responseBody")
+                                    myStringBuilder.append("Stop: $stopName ($stopNo) \n")
+                                    myStringBuilder.append("Route no.$routeNo, \nDirection: $route\n")
+
+                                    for (trips in routes.Trips.Trip) {
+                                        val currentDate = Calendar.getInstance()
+                                        val lastUpdated = trips.AdjustmentAge
+                                        val ETA = trips.AdjustedScheduleTime
+                                        currentDate.add(Calendar.MINUTE, ETA.toInt())
+                                        myStringBuilder.append(
+                                            "Next arrival: $ETA minutes (${
+                                                sdf.format(
+                                                    currentDate.time
+                                                )
+                                            })\n"
+                                        )
+                                        if (lastUpdated != "-1") {
+                                            val lastUpdatedDouble = lastUpdated.toDouble()
+                                            val lastUpdatedSecond =
+                                                round(lastUpdatedDouble * 60).toInt()
+                                            myStringBuilder.append("Last updated: $lastUpdatedSecond seconds ago\n")
+
+                                        }
+                                    }
+
+                                }
+                            } catch (ex: Exception) {
+                                myStringBuilder.append("Something went wrong. Please try again later")
+                            }
+
+
+                        } else if (Route[18].toString() == "{") {
+                            try {
+
+                                Log.d("Route[18]", "NO")
+
+                                val data = Gson().fromJson(
+                                    responseBody.GetNextTripsForStopResult.Route,
+                                    ApiResponse::class.java
+                                )
+
+                                Log.d("data", "${data.RouteDirection}")
+                                val route = data.RouteDirection.RouteLabel
+                                val routeNo = data.RouteDirection.RouteNo
 
 
                                 Log.d("responseBody", "$responseBody")
-                                myStringBuilder.append("Stop: $stopName ($stopNo) \n" )
+                                myStringBuilder.append("Stop: $stopName ($stopNo) \n")
                                 myStringBuilder.append("Route no.$routeNo, \nDirection: $route\n")
-
-                                for(trips in routes.Trips.Trip){
+                                for (trips in data.RouteDirection.Trips.Trip) {
                                     val currentDate = Calendar.getInstance()
                                     val lastUpdated = trips.AdjustmentAge
                                     val ETA = trips.AdjustedScheduleTime
                                     currentDate.add(Calendar.MINUTE, ETA.toInt())
-                                    myStringBuilder.append("Next arrival: $ETA minutes (${sdf.format(currentDate.time)})\n")
-                                    if(lastUpdated != "-1") {
+                                    myStringBuilder.append(
+                                        "Next arrival: $ETA minutes (${
+                                            sdf.format(
+                                                currentDate.time
+                                            )
+                                        })\n"
+                                    )
+                                    if (lastUpdated != "-1") {
                                         val lastUpdatedDouble = lastUpdated.toDouble()
-                                        val lastUpdatedSecond = round(lastUpdatedDouble*60).toInt()
+                                        val lastUpdatedSecond =
+                                            round(lastUpdatedDouble * 60).toInt()
                                         myStringBuilder.append("Last updated: $lastUpdatedSecond seconds ago\n")
 
                                     }
                                 }
-
+                            } catch (ex: Exception) {
+                                myStringBuilder.append("Something went wrong. Please try again later.")
                             }
-                        }catch(ex: Exception){
-                            myStringBuilder.append("Something went wrong. Please try again later")
                         }
 
-
-                    }else if(Route[18].toString() == "{") {
-                        try {
-
-                            Log.d("Route[18]", "NO")
-
-                            val data = Gson().fromJson(
-                                responseBody.GetNextTripsForStopResult.Route,
-                                ApiResponse::class.java
-                            )
-
-                            Log.d("data", "${data.RouteDirection}")
-                            val route = data.RouteDirection.RouteLabel
-                            val routeNo = data.RouteDirection.RouteNo
-
-
-                            Log.d("responseBody", "$responseBody")
-                            myStringBuilder.append("Stop: $stopName ($stopNo) \n")
-                            myStringBuilder.append("Route no.$routeNo, \nDirection: $route\n")
-                            for (trips in data.RouteDirection.Trips.Trip) {
-                                val currentDate = Calendar.getInstance()
-                                val lastUpdated = trips.AdjustmentAge
-                                val ETA = trips.AdjustedScheduleTime
-                                currentDate.add(Calendar.MINUTE, ETA.toInt())
-                                myStringBuilder.append(
-                                    "Next arrival: $ETA minutes (${
-                                        sdf.format(
-                                            currentDate.time
-                                        )
-                                    })\n"
-                                )
-                                if (lastUpdated != "-1") {
-                                    val lastUpdatedDouble = lastUpdated.toDouble()
-                                    val lastUpdatedSecond = round(lastUpdatedDouble * 60).toInt()
-                                    myStringBuilder.append("Last updated: $lastUpdatedSecond seconds ago\n")
-
-                                }
-                            }
-                        }catch(ex: Exception){
-                            myStringBuilder.append("Something went wrong. Please try again later.")
-                        }
+                        myString = myStringBuilder
+                        updateText.text = myString
+                        Log.d("myStringBuilder", "$myStringBuilder")
                     }
 
-                    myString = myStringBuilder
-                    updateText.text = myString
-                    Log.d("myStringBuilder", "$myStringBuilder")
-                }
-
-                override fun onFailure(call: Call<BusAPI?>, t: Throwable) {
-                    TODO("TESTTTTTTTTT")
-                }
-            })
+                    override fun onFailure(call: Call<BusAPI?>, t: Throwable) {
+                        TODO("TESTTTTTTTTT")
+                    }
+                })
+            }
         }
 
         goButton.setOnClickListener{
